14f43fb84cf08f254a019c7ce0bf0e5f1fedf0a1
teezecrost <hello@rocketsciencedesigns.com>
2026-01-20 22:41:52 -0600

    fix summary column

--- apps/api/prisma/migrations/20260121041957_interaction_summary/migration.sql
+++ apps/api/prisma/migrations/20260121041957_interaction_summary/migration.sql
@@ -2,10 +2,10 @@
 ALTER TABLE "Interaction" ADD COLUMN     "summary" TEXT;
 
 -- AddForeignKey
-ALTER TABLE "SubscriberChannel" ADD CONSTRAINT "SubscriberChannel_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "Subscriber"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+--ALTER TABLE "SubscriberChannel" ADD CONSTRAINT "SubscriberChannel_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "Subscriber"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Interaction" ADD CONSTRAINT "Interaction_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "Subscriber"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+--ALTER TABLE "Interaction" ADD CONSTRAINT "Interaction_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "Subscriber"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UsageRollup" ADD CONSTRAINT "UsageRollup_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "Subscriber"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+--ALTER TABLE "UsageRollup" ADD CONSTRAINT "UsageRollup_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "Subscriber"("id") ON DELETE CASCADE ON UPDATE CASCADE;
